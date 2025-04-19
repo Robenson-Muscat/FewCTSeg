40f6fdf4c46c9ff2eb6081bb8dd2a8fe39ff4a4b
Robenson-Muscat <robensonmondelice@gmail.com>
2025-04-19 12:27:36 +0200

    Add New class Dataset for semantic segmentation and with 1 channel

--- src/dataset.py
+++ src/dataset.py
@@ -7,7 +7,7 @@ import numpy as np
 from PIL import Image
 import torch
 
-class CTDataset(Dataset):
+class CTVisuDataset(Dataset):
   def __init__(self,path,transform=None):
     self.filenames = sorted(glob.glob(os.path.join(path, '**/*.png'),recursive = True),key=alphanumeric_sort)
     self.transform = transform
@@ -55,4 +55,51 @@ class CTScanDataset(Dataset):
             mask = torch.tensor(mask)  # Convert to a Tensor
 
         return img, mask
+
+
+
+# ---------------------------
+# Chemins des données
+# ---------------------------
+PATH = "./data/"
+train_path = os.path.join(PATH, "train-images/")
+
+# ---------------------------
+# 1. Dataset Definition
+# ---------------------------
+class GrayCTScanDataset(Dataset):
+    def __init__(self, image_dir, mask_csv=None, transform=None):
+        self.image_dir = image_dir
+        self.image_paths = sorted(glob.glob(os.path.join(image_dir, "*.png")),key = alphanumeric_sort)
+        self.transform = transform
+        
+        if mask_csv is not None:
+            masks = pd.read_csv(mask_csv, index_col=0, header=0).T.values
+            self.masks = masks.reshape(-1, 256, 256).astype(np.int64)
+        else:
+            self.masks = None
+
+    def __len__(self):
+        return len(self.image_paths)
+
+    def __getitem__(self, idx):
+        img = Image.open(self.image_paths[idx]).convert("L")
+        img = np.array(img, dtype=np.float32)  
+        img_tensor = torch.from_numpy(img).unsqueeze(0)  # (1,H,W) 
+    
+
+        mask_tensor = None
+        if self.masks is not None:
+            mask = self.masks[idx]
+            mask_tensor = torch.from_numpy(mask)  # (H,W)
+            
+            # torch.Size([1, 256, 256]), dtype:  torch.int64
+            # Maybe it has to be mask of shape [256,256], dtype: uint8
+            #mask_tensor = mask_tensor.unsqueeze(0) #(1,H,W)
+
+
+        if self.transform:
+            img_tensor = self.transform(img_tensor)
+
+        return img_tensor, mask_tensor
 

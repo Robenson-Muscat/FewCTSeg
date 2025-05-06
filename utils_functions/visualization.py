c5c98c33fb415287d8dc30b7431bbd1468e087de
Robenson Mondelice <robensonmondelice@gmail.com>
2025-05-06 11:29:06 +0200

    Add some optionnal functions to visualize

--- utils_functions/visualization.py
+++ utils_functions/visualization.py
@@ -145,3 +145,34 @@ def visualize_augmentations(dataset, idx=0, samples=5):
 #elt = aug(image=originalImage)[‘image’]
 #cv2_imshow(elt)
 
+def visualize(image, mask, original_image=None, original_mask=None):
+    fontsize = 18
+
+    if original_image is None and original_mask is None:
+        f, ax = plt.subplots(2, 1, figsize=(8, 8))
+
+        ax[0].imshow(image)
+        ax[1].imshow(mask)
+    else:
+        f, ax = plt.subplots(2, 2, figsize=(8, 8))
+
+        ax[0, 0].imshow(original_image)
+        ax[0, 0].set_title('Original image', fontsize=fontsize)
+
+        ax[1, 0].imshow(original_mask)
+        ax[1, 0].set_title('Original mask', fontsize=fontsize)
+
+        ax[0, 1].imshow(image)
+        ax[0, 1].set_title('Transformed image', fontsize=fontsize)
+
+        ax[1, 1].imshow(mask)
+        ax[1, 1].set_title('Transformed mask', fontsize=fontsize)
+
+
+
+#image = cv2.imread('0fea4b5049_image.png')
+#mask = cv2.imread('0fea4b5049.png', cv2.IMREAD_GRAYSCALE)
+#print(image.shape, mask.shape)
+#original_height, original_width = image.shape[:2]
+#visualize(image, mask)
+

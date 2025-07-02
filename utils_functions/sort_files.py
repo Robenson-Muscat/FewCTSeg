0d6d262ae52b0c47e4748046433d7a9a2834fa64
Robenson Mondelice <robensonmondelice@gmail.com>
2025-07-02 15:16:09 +0200

    Add some comments on sort_files

--- utils_functions/sort_files.py
+++ utils_functions/sort_files.py
@@ -1,5 +1,7 @@
 import re
 
 def alphanumeric_sort(name):
+    """Sort filenames according a alphanumeric order"""
+    
     parts = re.split('(\d+)', name)
     return [int(part) if part.isdigit() else part for part in parts]

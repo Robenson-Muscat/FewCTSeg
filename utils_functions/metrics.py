cc19c35205b78320429f9512c35e708b59a4e9c5
Robenson-Muscat <robensonmondelice@gmail.com>
2025-07-21 15:30:53 +0200

    Add descriptions

--- utils_functions/metrics.py
+++ utils_functions/metrics.py
@@ -6,18 +6,44 @@ import pandas as pd
 NUM_CLASSES = 54
 
 def dice_image(prediction, ground_truth):
+    """
+    Compute the dice score for an image
+
+    Args:
+        ground_truth : Ground truth mask 
+        prediction : Prediction mask 
+    Returns :
+        Dice score for an image
+    """
+
     intersection = np.sum(prediction * ground_truth)
     if np.sum(prediction) == 0 and np.sum(ground_truth) == 0:
         return np.nan
     return 2 * intersection / (np.sum(prediction) + np.sum(ground_truth))
 
 def dice_multiclass(prediction, ground_truth):
+    """
+    Compute the dice score per class for an image
+
+    Args:
+        ground_truth : Ground truth mask 
+        prediction : Prediction mask 
+    Returns :
+        Array of dice score per class for an image
+    """
     dices = []
     for i in range(1, NUM_CLASSES + 1):
         dices.append(dice_image(prediction == i, ground_truth == i))
     return np.array(dices)
 
 def dice_pandas(y_true_df: pd.DataFrame, y_pred_df: pd.DataFrame) -> float:
+    """
+    Args:
+        y_true_df : Ground truth masks (pd.DataFrame)
+        y_pred_df : Prediction masks (pd.DataFrame)
+    Returns :
+        Dice score
+    """
     y_pred_df = y_pred_df.T
     y_true_df = y_true_df.T
     individual_dice = []
